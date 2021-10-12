4c08fb2d910c0b09dde13baeb04445aa9f3e145b
yuina86861 <76641185+yuina8686@users.noreply.github.com>
2021-10-12 06:30:27 +0000

    2nd

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ client.on('ready', () => {
 client.on('message', async msg => {
   if (msg.content === '!ping') {
     msg.channel.send('Pong!');
-  } else if (msg.content == '!uid' || msg.content === '!user_id') {
+  } else if (msg.content === '!uid' || msg.content === '!user_id') {
     msg.channel.send(msg.author.id);
-  } else if (msg.content == '!cid' || msg.content === '!channel_id') {
+  } else if (msg.content === '!cid' || msg.content === '!channel_id') {
     msg.channel.send(msg.channel.id);
-  } else if (msg.content == '!gid' || msg.content === '!guild_id' || msg.content === '!server_id' || msg.content === '!sid') {
+  } else if (msg.content === '!gid' || msg.content === '!guild_id' || msg.content === '!server_id' || msg.content === '!sid') {
     msg.channel.send(msg.guild.id);
+  } else if (msg.content.startsWith('!contact')) {
+    command = msg.content.split(' ');
+    if (command.length === 2) {
+      user = client.users.fetch('802152878855684106');
+      user.send(command[1]);
+      msg.channel.send('送信しました。');
+    } else {
+      msg.channel.send('引数が無効です');
+    }
   }
 });
 
