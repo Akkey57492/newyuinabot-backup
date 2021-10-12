41204486f1eb304dbba270783cab08cde0f1ddbc
yuina86861 <76641185+yuina8686@users.noreply.github.com>
2021-10-12 06:11:21 +0000

    1st

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ client.on('ready', () => {
 client.on('message', async msg => {
   if (msg.content === '!ping') {
     msg.channel.send('Pong!');
-  } else if (msg.content == '!uid') {
+  } else if (msg.content == '!uid' || msg.content === '!user_id') {
     msg.channel.send(msg.author.id);
-  } else if (msg.content == '!cid') {
+  } else if (msg.content == '!cid' || msg.content === '!channel_id') {
     msg.channel.send(msg.channel.id);
-  } else if (msg.content == '!gid') {
+  } else if (msg.content == '!gid' || msg.content === '!guild_id' || msg.content === '!server_id' || msg.content === '!sid') {
     msg.channel.send(msg.guild.id);
   }
 });
